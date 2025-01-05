d8d94a3e91d92ad2bab091ec02c9ba90cdfc405f
Ankush <ankushnulake70@gmail.com>
2025-01-05 13:04:15 +0530

    First commit

---
+++ src/test/java/GitPractice/Demo.java
@@ -0,0 +1,14 @@
+package GitPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+
+    @Test
+    public void demoTest(){
+
+        System.out.println("git practice");
+
+    }
+}
